99d8d31a3bd41b69a5502be92f61c9b05bdbae64
wangyinda <ran2sky@gmail.com>
2024-12-08 18:14:02 +0800

    fix: gum arch check fix

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,8 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
 GUM_VERSION="0.14.3"
-ARCH=$(dpkg --print-architecture | sed 's/arm64/aarch64/')
+ARCH=$(dpkg --print-architecture)
+echo "Installing Gum for architecture: $ARCH"
 # Use known good version
 wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_${ARCH}.deb"
 sudo apt-get install -y ./gum.deb
