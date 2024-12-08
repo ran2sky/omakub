f675a57d38f211a43a0ce5c5c7e5e3af67701a70
wangyinda <ran2sky@gmail.com>
2024-12-08 22:40:01 +0800

    fix: chrome add arch check

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -1,8 +1,15 @@
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
 cd /tmp
-ARCH=$(dpkg --print-architecture) && \
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_${ARCH}.deb
-sudo apt install -y ./google-chrome-stable_current_${ARCH}.deb
-rm google-chrome-stable_current_${ARCH}.deb
-xdg-settings set default-web-browser google-chrome.desktop
+ARCH=$(dpkg --print-architecture)
+if [ "$ARCH" == "amd64" ]; then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_${ARCH}.deb
+    sudo apt install -y ./google-chrome-stable_current_${ARCH}.deb
+    rm google-chrome-stable_current_${ARCH}.deb
+    xdg-settings set default-web-browser google-chrome.desktop
+elif [ "$ARCH" == "arm64" ]; then
+    sudo snap install chromium
+    xdg-settings set default-web-browser chromium_chromium.desktop
+else
+    echo "Unsupported architecture: $ARCH"
+fi
 cd -
--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,9 +1,9 @@
 OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
-DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
+DEFAULT_OPTIONAL_APPS='1password,Spotify'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="Node.js", "Go","PHP", "Python", "Java"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
