82a5be0658ea52dc4400ec54dbe3cf9bcfffcafc
wangyinda <ran2sky@gmail.com>
2024-12-08 23:07:02 +0800

    fix: add app arch check

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,5 +1,6 @@
 cd /tmp
-wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+ARCH=$(dpkg --print-architecture | sed 's/arm64/aarch64/')
+wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-${ARCH}-unknown-linux-musl.tar.gz"
 tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
