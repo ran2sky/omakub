72066da88683b53c677e1097e24e3bb6a3bfba11
babarot <b4b4r07@gmail.com>
2025-01-03 15:29:14 +0800

    add restart docker

--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -12,4 +12,18 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo usermod -aG docker ${USER}
 
 # Limit log size and add registry mirror
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"},"registry-mirrors":["https://hammal.iwliw.com"]}' | sudo tee /etc/docker/daemon.json
+cat << 'EOF' | sudo tee /etc/docker/daemon.json
+{
+  "registry-mirrors": [
+    "https://hammal.iwliw.com"
+  ],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "10m",
+    "max-file": "5"
+  }
+}
+EOF
+
+# Restart Docker
+sudo systemctl restart docker
